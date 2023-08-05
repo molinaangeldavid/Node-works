25e09320e8f5f07f852086feb82a1f7a488ead1d
David Molina <molina.angeldavid@gmail.com>
2023-08-05 16:02:36 -0300

    change something

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -4,13 +4,13 @@ const crearTabla = (base = 5,listar = false, limite = 10) => {
     
 
     return new Promise( (resolve, reject) => {
-        console.log("Tabla de " + base, '\n');
+        console.log("TABLA DE MULTIPLICACION: " + base, '\n');
         let salida = '';
         
         for(let i = 1 ; i <= limite ;i++){
             salida += `${base} x ${i} = ${base*i}\n`;
         }
-        
+
         if(listar){
             console.log(salida)
         }
